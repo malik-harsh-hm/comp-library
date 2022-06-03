2118dc4953c0054ebb3c545ebb66c56c6f455bb2
HarshMalik <harsh.malik@akqa.com>
2022-06-03 13:54:04 +0530

    Parser updated to read Material UI Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button} from './components/button/Button'
+import {Button} from './components/ButtonComponent'
 import { stringify } from 'querystring';
 
 function App() {
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from '../components/button/Button';
+import { Button } from '../components/ButtonComponent';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
